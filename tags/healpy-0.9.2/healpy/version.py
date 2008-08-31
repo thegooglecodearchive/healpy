53478876ada06ee847b3df97ad3fe2f4edb13714
cyrille.rosset@gmail.com <cyrille.rosset@gmail.com@b20c88ee-9555-0410-a987-f1abe7a6e862>
2008-08-31 17:17:57 +0000

    Initialimport of last tagged version

---
+++ tags/healpy-0.9.2/healpy/version.py
@@ -0,0 +1,7 @@
+rev  = "$Revision: 113 $"
+
+revnumber = int(rev.strip('$')[9:])
+date = "$Date: 2008-07-20 17:51:13 +0200 (dim, 20 jui 2008) $"
+
+#__version__='trunk-r%d'%(revnumber)
+__version__ = '0.9.2'
