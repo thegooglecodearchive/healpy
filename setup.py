3e1d6a578ed946d1939884a71d500ad3fb66e225
martin.reinecke1@gmx.de <martin.reinecke1@gmx.de@b20c88ee-9555-0410-a987-f1abe7a6e862>
2010-11-04 15:09:33 +0000

    new targets for C++ library, fix typos

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import platform
 
 TARGET_DICT = {
-    'linux': 'generic_gcc',
-    'darwin': 'osx'
+    'linux': 'healpy',
+    'darwin': 'healpy'
 }
 
 SYSTEM_STRING = platform.system().lower ()
@@ -62,13 +62,13 @@ if sys.argv[1] != 'sdist':
     compile_healpix_cxx(HEALPIX_TARGET)
     if not ( isdir(healpix_cxx_dir+'/include') and
              isdir(healpix_cxx_dir+'/lib') ):
-        raise IOError("No inlcude and lib directory : needed for healpy !")
+        raise IOError("No include and lib directory : needed for healpy !")
 
     if HEALPIX_TARGET2 != HEALPIX_TARGET:
         compile_healpix_cxx(HEALPIX_TARGET2)
         if not ( isdir(healpix_cxx_dir2+'/include') and
                  isdir(healpix_cxx_dir2+'/lib') ):
-            raise IOError("No inlcude and lib directory : needed for healpy !")
+            raise IOError("No include and lib directory : needed for healpy !")
 
 ###############################################
 
